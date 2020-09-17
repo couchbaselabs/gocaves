0d54bd9a762cb9f71914812e6e1ba0fbca2fc6f9
Brett Lawson <brett19@gmail.com>
2020-09-16 18:30:07 -0700

    Updated check-get test description to be clearer.

--- checksuite/crud/check_get.go
+++ checksuite/crud/check_get.go
@@ -8,31 +8,19 @@ import (
 // operations appropriately.
 /*
 Should be implemented as:
-	type TestDoc struct {
-		Foo string
-		Bar bool
+	@CavesTest('core/SetGet')
+	@CavesRequireFeature('replicas')
+	function testCoreSetGet(t *TestCase) {
+		collection := t.Collection()
+
+		await collection.upsert('testdoc', {
+			foo: 'hello',
+			bar: true,
+		})
+
+		doc := await collection.get('testdoc')
+		assert.deepEqual()
 	}
-	t := harness.StartTest("core/SetGet")
-	t.RequireFeature("replicas")
-	collection := t.Collection() //
-
-	testValOut := TestDoc{
-		Foo: "hello",
-		Bar: true,
-	}
-
-	_, err := collection.Upsert("testdoc", GetOptions{
-
-	}, testValOut)
-	assert(err == nil)
-
-	doc, err := collection.Get("testdoc", GetOptions{})
-	assert(err == nil)
-
-	testValIn := TestDoc{}
-	doc.Value(&testValIn)
-
-	t.End()
 */
 func CheckSetGet(t *checkregistry.T) {
 	//t.RequireFeature(checkregistry.FeatureReplicas)
