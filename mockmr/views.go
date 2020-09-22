7e327c14d0513942556856d9a2ac7f800af7b759
Brett Lawson <brett19@gmail.com>
2020-09-22 11:11:47 -0700

    Added some comments so I stop seeing yellow lines.

--- mockmr/views.go
+++ mockmr/views.go
@@ -2,23 +2,28 @@ package mockmr
 
 import "github.com/couchbaselabs/gocaves/mockdb"
 
+// Index represents a single map reduce query.
 type Index struct {
 	MapFunc    string
 	ReduceFunc string
 }
 
+// Engine represents the mock map reduce engine.
 type Engine struct {
 	Indexes []Index
 }
 
+// ExecuteOptions provides options when executing an query.
 type ExecuteOptions struct {
 	Data *mockdb.Bucket
 }
 
+// ExecuteResults provides the results from an executed query.
 type ExecuteResults struct {
 	Rows []interface{}
 }
 
+// Execute executes a query.
 func (e *Engine) Execute(opts ExecuteOptions) (*ExecuteResults, error) {
 	return nil, nil
 }
--- mockn1ql/query.go
+++ mockn1ql/query.go
@@ -2,23 +2,28 @@ package mockn1ql
 
 import "github.com/couchbaselabs/gocaves/mockdb"
 
+// Index represents a single view index.
 type Index struct {
 	BucketName string
 	Fields     []string
 }
 
+// Engine represents the mock query engine.
 type Engine struct {
 	Indexes []*Index
 }
 
+// ExecuteOptions provides options when executing an query.
 type ExecuteOptions struct {
 	Data map[string]*mockdb.Bucket
 }
 
+// ExecuteResults provides the results from an executed query.
 type ExecuteResults struct {
 	Rows []interface{}
 }
 
+// Execute executes a query.
 func (e *Engine) Execute(opts ExecuteOptions) (*ExecuteResults, error) {
 	return nil, nil
 }
