fb0c60b6b7fbbbc418a9684ad5b3a2920c46a405
Brett Lawson <brett19@gmail.com>
2021-02-18 23:19:50 -0800

    #35: Improved handling for KV HELLO operations.

--- mock/mockimpl/svcimpls/kvimplhello.go
+++ mock/mockimpl/svcimpls/kvimplhello.go
@@ -15,6 +15,37 @@ func (x *kvImplHello) Register(h *hookHelper) {
 }
 
 func (x *kvImplHello) handleHelloRequest(source mock.KvClient, pak *memd.Packet) {
+	isInFeatureList := func(features []memd.HelloFeature, feature memd.HelloFeature) bool {
+		for _, foundFeature := range features {
+			if foundFeature == feature {
+				return true
+			}
+		}
+		return false
+	}
+
+	// TODO(brett19): This list should actually be moderated by the server
+	// version/features configured at the top level for the cluster.
+	availableFeatures := []memd.HelloFeature{
+		memd.FeatureDatatype,
+		memd.FeatureTCPNoDelay,
+		memd.FeatureSeqNo,
+		memd.FeatureTCPDelay,
+		memd.FeatureXattr,
+		memd.FeatureXerror,
+		memd.FeatureSelectBucket,
+		memd.FeatureSnappy,
+		memd.FeatureJSON,
+		memd.FeatureDuplex,
+		//memd.FeatureClusterMapNotif,
+		memd.FeatureUnorderedExec,
+		//memd.FeatureDurations,
+		//memd.FeatureAltRequests,
+		//memd.FeatureSyncReplication,
+		memd.FeatureCollections,
+		//memd.FeatureOpenTracing,
+		//memd.FeatureCreateAsDeleted,
+	}
 	enabledFeatures := make([]memd.HelloFeature, 0)
 
 	numFeatures := len(pak.Value) / 2
@@ -22,10 +53,14 @@ func (x *kvImplHello) handleHelloRequest(source mock.KvClient, pak *memd.Packet)
 		featureCodeID := binary.BigEndian.Uint16(pak.Value[featureIdx*2:])
 		featureCode := memd.HelloFeature(featureCodeID)
 
-		if featureCode == memd.FeatureDatatype {
-			enabledFeatures = append(enabledFeatures, featureCode)
+		if isInFeatureList(availableFeatures, featureCode) {
+			if !isInFeatureList(enabledFeatures, featureCode) {
+				enabledFeatures = append(enabledFeatures, featureCode)
+			}
 		}
 	}
+
+	source.SetFeatures(enabledFeatures)
 
 	enabledBytes := make([]byte, len(enabledFeatures)*2)
 	for featureIdx, featureCode := range enabledFeatures {
