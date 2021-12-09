cb8936676bcc413146ea100b56893f41715be0a1
Charles Dixon <chvckd@gmail.com>
2021-12-09 10:26:27 +0000

    Ensure server duration is never sent as zero.

--- mock/mockimpl/svcimpls/utils.go
+++ mock/mockimpl/svcimpls/utils.go
@@ -2,6 +2,7 @@ package svcimpls
 
 import (
 	"log"
+	"math/rand"
 	"time"
 
 	"github.com/couchbase/gocbcore/v9/memd"
@@ -10,8 +11,15 @@ import (
 
 func writePacketToSource(source mock.KvClient, pak *memd.Packet, start time.Time) {
 	if source.HasFeature(memd.FeatureDurations) {
+		// TODO (chvck): revisit this, for some reason Windows reports a server duration of 0.
+		// Golang time accuracy in Windows is good enough that this shouldn't be the case and it seems pretty unlikely
+		// that we've actually done the operation in no time.
+		duration := time.Since(start)
+		if duration == 0 {
+			duration = time.Duration(rand.Intn(200)+1) * time.Microsecond
+		}
 		pak.ServerDurationFrame = &memd.ServerDurationFrame{
-			ServerDuration: time.Since(start),
+			ServerDuration: duration,
 		}
 	}
 	err := source.WritePacket(pak)
