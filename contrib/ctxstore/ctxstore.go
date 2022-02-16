6c3e0913d3d8560d01c666a77211c00587831871
JRasagneres <jacquesrascagneres@gmail.com>
2022-02-16 17:21:16 +0000

    Add mutex to lock store on read / write

--- contrib/ctxstore/ctxstore.go
+++ contrib/ctxstore/ctxstore.go
@@ -2,11 +2,13 @@ package ctxstore
 
 import (
 	"reflect"
+	"sync"
 )
 
 // Store allows generic contextual data storage
 type Store struct {
-	data map[reflect.Type]reflect.Value
+	data     map[reflect.Type]reflect.Value
+	dataLock sync.Mutex
 }
 
 // Get returns an associated context by its type
@@ -22,6 +24,8 @@ func (s *Store) Get(valuePtr interface{}) {
 	}
 
 	vType := ptrV.Type().Elem()
+	s.dataLock.Lock()
+	defer s.dataLock.Unlock()
 	if s.data != nil {
 		if ctx, ok := s.data[vType]; ok {
 			ptrV.Set(ctx)
